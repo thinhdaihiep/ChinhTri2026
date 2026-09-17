f4de51b9b68295d269f74963c1e2a704caf1c43f
thinhdaihiep <60024932+thinhdaihiep@users.noreply.github.com>
2026-09-17 16:23:39 +0700

    feat: update quiz questions from JSON files
    Replace hardcoded question arrays with imported JSON datasets to improve data management and scalability.

--- src/data/defaultQuestions.ts
+++ src/data/defaultQuestions.ts
@@ -1,12 +1,25 @@
 import { QuestionItem } from '../types';
-import { questions1 } from './questions1';
-import { questions2 } from './questions2';
-import { questions3 } from './questions3';
-import { questions4 } from './questions4';
+import q1 from './q_001_100.json';
+import q2 from './q_101_200.json';
+import q3 from './q_201_300.json';
+import q4 from './q_301_400.json';
+import q5 from './q_401_500.json';
+import q6 from './q_501_600.json';
+import q7 from './q_601_700.json';
+import q8 from './q_701_800.json';
+import q9 from './q_801_900.json';
+import q10 from './q_901_1000.json';
 
 export const defaultQuestions: QuestionItem[] = [
-  ...questions1,
-  ...questions2,
-  ...questions3,
-  ...questions4
+  ...(q1 as QuestionItem[]),
+  ...(q2 as QuestionItem[]),
+  ...(q3 as QuestionItem[]),
+  ...(q4 as QuestionItem[]),
+  ...(q5 as QuestionItem[]),
+  ...(q6 as QuestionItem[]),
+  ...(q7 as QuestionItem[]),
+  ...(q8 as QuestionItem[]),
+  ...(q9 as QuestionItem[]),
+  ...(q10 as QuestionItem[])
 ];
+
